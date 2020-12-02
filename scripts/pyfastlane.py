2450cdfeb5cd8c8b45dc4d13e72e72f41def75f8
Ed Sanville <edsanville@gmail.com>
2020-12-02 14:29:56 -0500

    Add snapshot support

--- scripts/pyfastlane.py
+++ scripts/pyfastlane.py
@@ -4,6 +4,8 @@ import configparser
 import sys
 import os
 import json
+import tempfile
+import glob
 
 
 '''Executes and prints a command'''
@@ -28,6 +30,10 @@ class App:
         self.connect_username = connect['username']
         self.connect_team_name = connect['team_name']
 
+        screenshots = config['screenshots']
+        self.screenshot_languages = [x.strip() for x in screenshots['languages'].split(',')]
+        self.screenshot_devices = [x.strip() for x in screenshots['devices'].split(',')]
+
         submission_information_string = json.dumps({
             'export_compliance_uses_encryption': self.uses_encryption,
             'add_id_info_uses_idfa': self.uses_idfa
@@ -38,12 +44,17 @@ class App:
         self.actions = {
             'increment_build_number': self.increment_build_number,
             'increment_patch_number': self.increment_patch_number,
+            'increment_minor_version': self.increment_minor_version,
+            'increment_major_version': self.increment_major_version,
             'build': self.build,
             'upload_binary': self.upload_binary,
             'upload_metadata': self.upload_metadata,
+            'upload_screenshots': self.upload_screenshots,
+            'replace_screenshots': self.replace_screenshots,
             'testflight': self.testflight,
             'submit': self.submit,
             'release': self.release,
+            'snapshot': self.snapshot,
             'help': self.help
         }
 
@@ -59,27 +70,47 @@ class App:
 
     def increment_build_number(self):
         '''Increments the build number of the project'''
-        execute(f'bundle exec fastlane run increment_build_number xcodeproj:"{self.project}"')
+        execute(f'fastlane run increment_build_number xcodeproj:"{self.project}"')
 
 
     def increment_patch_number(self):
         '''Increments the patch number of the project (e.g. 3.2.x)'''
-        execute(f'bundle exec fastlane run increment_version_number bump_type:patch xcodeproj:"{self.project}"')
+        execute(f'fastlane run increment_version_number bump_type:patch xcodeproj:"{self.project}"')
+
+
+    def increment_minor_version(self):
+        '''Increments the minor version of the project (e.g. 3.x.1)'''
+        execute(f'fastlane run increment_version_number bump_type:minor xcodeproj:"{self.project}"')
+
+
+    def increment_major_version(self):
+        '''Increments the major version of the project (e.g. x.2.1)'''
+        execute(f'fastlane run increment_version_number bump_type:major xcodeproj:"{self.project}"')
 
 
     def build(self):
         '''Builds the .ipa file'''
-        execute(f'bundle exec fastlane gym --workspace {self.workspace} --scheme {self.scheme}')
+        execute(f'fastlane gym --workspace {self.workspace} --scheme {self.scheme}')
 
 
     def upload_binary(self):
         '''Uploads the .ipa file to App Store Connect'''
-        execute(f'bundle exec fastlane deliver {self.deliver_options} --skip_screenshots --skip_metadata')
+        execute(f'fastlane deliver {self.deliver_options} --skip_screenshots --skip_metadata')
 
 
     def upload_metadata(self):
         '''Uploads the metadata to App Store Connect'''
-        execute(f'bundle exec fastlane deliver {self.deliver_options} --skip_binary_upload --skip_screenshots')
+        execute(f'fastlane deliver {self.deliver_options} --skip_binary_upload --skip_screenshots')
+
+
+    def upload_screenshots(self):
+        '''Uploads screenshots to App Store Connect'''
+        execute(f'fastlane deliver {self.deliver_options} --skip_binary_upload --skip_metadata --force')
+
+
+    def replace_screenshots(self):
+        '''Replace all screenshots to App Store Connect'''
+        execute(f'fastlane deliver {self.deliver_options} --skip_binary_upload --skip_metadata --force --overwrite_screenshots')
 
 
     def testflight(self):
@@ -91,14 +122,43 @@ class App:
 
     def submit(self):
         '''Submits the latest build for the latest version number on App Store Connect'''
-        execute(f'bundle exec fastlane deliver submit_build {self.deliver_options} --skip_screenshots --skip_metadata')
+        execute(f'fastlane deliver submit_build {self.deliver_options} --skip_screenshots --skip_metadata')
 
 
     def release(self):
         '''Increments build number, builds the .ipa file, uploads the metadata and .ipa file, and submits for release on App Store Connect'''
         self.increment_build_number()
         self.build()
-        execute(f'bundle exec fastlane deliver {self.deliver_options} --submit_for_review --skip_screenshots')
+        execute(f'fastlane deliver {self.deliver_options} --submit_for_review --skip_screenshots')
+
+    def snapshot(self):
+        '''Capture screenshots using Snapshot'''
+        deviceList = ",".join(self.screenshot_devices)
+
+        derived_data_dir = 'DerivedData'
+        os.makedirs(derived_data_dir, exist_ok=True)
+
+        # Build the app bundle once
+        device = self.screenshot_devices[0]
+        execute(f'xcodebuild -workspace {self.workspace} -scheme {self.scheme} -derivedDataPath {derived_data_dir} -destination "platform=iOS Simulator,name={device},OS=14.2" FASTLANE_SNAPSHOT=YES FASTLANE_LANGUAGE=en-US build-for-testing')
+
+        for device in self.screenshot_devices:
+            for language in self.screenshot_languages:
+                # Skip if we already have >4 screenshots in this directory
+                if len(glob.glob(f'fastlane/screenshots/{language}/{device}*')) > 4:
+                    print(f'Skipped {device:40}    {language:6}')
+                    continue
+
+                if language == 'no':
+                    language = 'no-NO'
+
+                execute(f'nice -n 20 fastlane run snapshot workspace:"{self.workspace}" scheme:"{self.scheme}" devices:"{device}" languages:"{language}" test_without_building:true derived_data_path:"{derived_data_dir}"')
+
+                # Sigh, we need to move "no-NO" to "no"
+                if language == 'no-NO':
+                    execute('rsync -r fastlane/screenshots/no-NO fastlane/screenshots/no')
+                    execute('rm -rf fastlane/screenshots/no-NO')
+
 
     def help(self):
         '''Shows available actions'''
@@ -118,4 +178,3 @@ if len(actions) == 0:
 
 for action in actions:
     app.doAction(action)
-
